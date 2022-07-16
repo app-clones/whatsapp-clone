904690f57392ba7166eb0421633e158cd905da64
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2022-07-15 17:20:19 -0700

    :sparkles: Web Chat App Bar

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:whatsapp_clone/widgets/contacts_list.dart';
+import 'package:whatsapp_clone/widgets/web_chat_appbar.dart';
 import 'package:whatsapp_clone/widgets/web_profile_bar.dart';
 import 'package:whatsapp_clone/widgets/web_search_bar.dart';
 
@@ -30,6 +31,13 @@ class WebScreenLayout extends StatelessWidget {
                 image: AssetImage("assets/backgroundImage.png"),
                 fit: BoxFit.cover,
               ),
+            ),
+            child: Column(
+              children: const [
+                WebChatAppbar(),
+                // Chat list
+                // Message input
+              ],
             ),
           ),
         ],
---
+++ lib/widgets/web_chat_appbar.dart
@@ -0,0 +1,47 @@
+import "package:flutter/material.dart";
+import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/data.dart';
+
+class WebChatAppbar extends StatelessWidget {
+  const WebChatAppbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.077,
+      width: MediaQuery.of(context).size.width * 0.75,
+      padding: const EdgeInsets.all(10),
+      color: webAppBarColor,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              const CircleAvatar(
+                backgroundImage: NetworkImage(
+                  "https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0",
+                ),
+                radius: 30,
+              ),
+              SizedBox(width: MediaQuery.of(context).size.width * 0.01),
+              Text(
+                data[0]["name"].toString(),
+                style: const TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+          Row(children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search, color: Colors.grey),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.more_vert, color: Colors.grey),
+            ),
+          ]),
+        ],
+      ),
+    );
+  }
+}
