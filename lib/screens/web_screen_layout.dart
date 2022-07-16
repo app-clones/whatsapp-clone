d05a160c7c3fd7ffd24041fd10338db1d59a16e7
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2022-07-15 17:07:02 -0700

    :sparkles: Web Search Bar

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:whatsapp_clone/widgets/contacts_list.dart';
 import 'package:whatsapp_clone/widgets/web_profile_bar.dart';
+import 'package:whatsapp_clone/widgets/web_search_bar.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
@@ -16,7 +17,7 @@ class WebScreenLayout extends StatelessWidget {
               child: Column(
                 children: const [
                   WebProfileBar(),
-                  // Web search bar,
+                  WebSearchBar(),
                   ContactsList(),
                 ],
               ),
